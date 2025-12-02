718ad2181986ad46f8aec380ad9497a34bc70438
Chris Bennett <110393183+oev-chrisbennett@users.noreply.github.com>
2025-12-02 07:56:39 +0000

    chore: scaffold 2

---
+++ src/02/index.ts
@@ -0,0 +1,10 @@
+const solve = async (input: string) => {
+    const lines = input.split("\n").map((line) => line.trim())
+    console.log(lines)
+    return {
+        partA: "-",
+        partB: "-",
+    }
+}
+
+export default solve
