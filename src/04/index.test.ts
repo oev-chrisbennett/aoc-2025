4e0cd02eb22c38c18aac9809bed5aad6ac56e64c
Chris Bennett <110393183+oev-chrisbennett@users.noreply.github.com>
2025-12-09 10:56:05 +0000

    chore: scaffold 4

---
+++ src/04/index.test.ts
@@ -0,0 +1,10 @@
+import { describe, expect, test } from "vitest"
+import solve from "./index"
+
+describe("day 04", () => {
+    test("partA: example", async () => {
+        const input = ""
+        const result = await solve(input)
+        expect(result.partA).toEqual(0)
+    })
+})
---
+++ src/04/index.ts
@@ -0,0 +1,11 @@
+const solve = async (
+    input: string,
+): Promise<{ partA: number; partB: number }> => {
+    console.debug(input)
+    return {
+        partA: 0,
+        partB: 0,
+    }
+}
+
+export default solve
